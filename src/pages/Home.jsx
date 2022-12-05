cd80402dead4f85feeeb31d22ca8a12c14c9db91
Coel Allen <coelallen@gmail.com>
2022-12-05 10:04:40 -0700

    naming changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import Veggie from "../components/Veggie";
-import Popular from "../components/Popular";
 import { motion } from "framer-motion";
 
 import React from 'react'
+import VegetarianRecipes from "../components/VegetarianRecipes";
+import PopularRecipes from "../components/PopularRecipes";
 
 function Home() {
   return (
@@ -11,8 +11,8 @@ function Home() {
       initial={{opacity: 0}}
       exit={{ opacity: 0 }}
       transition={{duration: 0.5}}>
-      <Veggie />
-      <Popular />
+      <VegetarianRecipes />
+      <PopularRecipes />
     </motion.div>
   )
 }
